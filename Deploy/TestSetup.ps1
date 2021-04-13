8f469efd8b95249fb5df51c2a601683ee9013f37
Brad Stevens <brad.stevens@gmail.com>
2021-04-13 13:12:39 -0600

    remove_usmtpath

--- Deploy/TestSetup.ps1
+++ Deploy/TestSetup.ps1
@@ -14,13 +14,12 @@ if (Get-Module -ListAvailable -Name pester) {
 
 #USMT & VC Variables
 $jcAdmuTempPath = 'C:\Windows\Temp\JCADMU\'
-$usmtTempPath = 'C:\Windows\Temp\JCADMU\USMT\'
 $msvc2013x64File = 'vc_redist.x64.exe'
 $msvc2013x86File = 'vc_redist.x86.exe'
 $msvc2013x86Link = 'http://download.microsoft.com/download/0/5/6/056dcda9-d667-4e27-8001-8a0c6971d6b1/vcredist_x86.exe'
 $msvc2013x64Link = 'http://download.microsoft.com/download/0/5/6/056dcda9-d667-4e27-8001-8a0c6971d6b1/vcredist_x64.exe'
-$msvc2013x86Install = "$usmtTempPath$msvc2013x86File /install /quiet /norestart"
-$msvc2013x64Install = "$usmtTempPath$msvc2013x64File /install /quiet /norestart"
+$msvc2013x86Install = "$jcAdmuTempPath$msvc2013x86File /install /quiet /norestart"
+$msvc2013x64Install = "$jcAdmuTempPath$msvc2013x64File /install /quiet /norestart"
 
 # JumpCloud Agent Installation Variables
 $AGENT_PATH = "${env:ProgramFiles}\JumpCloud"
@@ -68,8 +67,5 @@ if (Test-Path 'C:\Program Files\JumpCloud') {
 #install jcagent and prereq
 if (!(Test-path $jcAdmuTempPath)) {
     new-item -ItemType Directory -Force -Path $jcAdmuTempPath
-  }
-  if (!(Test-path $usmtTempPath)){
-    new-item -ItemType Directory -Force -Path $usmtTempPath
   }
     DownloadAndInstallAgent -msvc2013x64link:($msvc2013x64Link) -msvc2013path:($jcAdmuTempPath) -msvc2013x64file:($msvc2013x64File) -msvc2013x64install:($msvc2013x64Install) -msvc2013x86link:($msvc2013x86Link) -msvc2013x86file:($msvc2013x86File) -msvc2013x86install:($msvc2013x86Install)
