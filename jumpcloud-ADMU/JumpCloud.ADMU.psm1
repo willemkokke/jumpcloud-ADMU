394be72e92fa5d5a771d6159ff2a15f6ee3d3621
Brad Stevens <brad.stevens@gmail.com>
2021-04-20 17:43:36 -0600

    start-migrationreboot

--- jumpcloud-ADMU/JumpCloud.ADMU.psm1
+++ jumpcloud-ADMU/JumpCloud.ADMU.psm1
@@ -2,4 +2,4 @@ $Public = @(Get-ChildItem -Path "$PSScriptRoot\Powershell\Start-Migration.ps1")
 
 . "$PSScriptRoot\Powershell\Start-Migration.ps1"
 
-Export-ModuleMember -Function $Public.BaseName
+Export-ModuleMember -Function $Public.BaseName, Start-MigrationReboot
